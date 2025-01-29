e2e8f91c9a58fe9703dda9acf2414e2f024a480d
Alan <alan.alexis.ht@gmail.com>
2025-01-29 22:10:32 +0100

    Commit del 29/01/2025. Servicio inacabado.

--- src/app/service/mascota.service.ts
+++ src/app/service/mascota.service.ts
@@ -25,7 +25,7 @@ export interface Mascota {
 })
 export class MascotaService {
 
-  private url = "http://localhost:4040/";
+  private url = "http://localhost:4040";
 
   private mascotaSubject = new BehaviorSubject<Mascota[]>([]);
   mascota$ = this.mascotaSubject.asObservable();
@@ -35,21 +35,25 @@ export class MascotaService {
   }
 
   verTodasMascotas(): Observable<Mascota[]> {
-    return this.httpClient.get<Mascota[]>(`${this.url}`);
+    return this.httpClient.get<Mascota[]>(`${this.url}/listaMascotas`);
+  }
+
+  verTodasMascotasPorTipo(tipo : Tipo): Observable<Mascota[]> {
+    return this.httpClient.get<Mascota[]>(`${this.url}/listaMascotas?tipo${tipo}`);
   }
 
   verUnaMascota(id: number): Observable<Mascota> {
-    return this.httpClient.get<Mascota>(`${this.url}/${id}`);
+    return this.httpClient.get<Mascota>(`${this.url}/mascotas/${id}`);
   }
 
   guardarMascota(mascota: Mascota): Observable<Mascota> {
-    return this.httpClient.post<Mascota>(`${this.url}`, mascota)
+    return this.httpClient.post<Mascota>(`${this.url}/mascota`, mascota)
       .pipe(tap(() => this.verTodasMascotas())
       );
   }
 
   eliminarMascota(id: number): Observable<Mascota> {
-    return this.httpClient.delete<Mascota>(`${this.url}/${id}`)
+    return this.httpClient.delete<Mascota>(`${this.url}/mascotas/${id}`)
       .pipe(tap(() => this.verTodasMascotas().subscribe()));
   }
 }
